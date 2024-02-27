b81b264ad76e52ac4ca431abe560bfdc66beaf53
Eduardo da Silva Sichelero <135671476+eduardosichelero@users.noreply.github.com>
2024-02-27 13:54:05 -0300

    Pesquisa Binaria renomeada

--- Pesquisa/pesquisaBinaria.py
+++ Pesquisa/pesquisaBinaria.py
@@ -1,20 +1,20 @@
-def binary_search(lst, item):
-    low = 0
-    high = len(lst) - 1
+def BuscaBinaria(lst, item):
+    baixo = 0
+    alto = len(lst) - 1
 
-    while low <= high:
-        middle = (high + low) // 2
-        kick = lst[middle]
-        if kick == item:
-            return middle
-        if kick > item:
-            high = middle - 1
+    while baixo <= alto:
+        meio = (alto + baixo) // 2
+        chute = lst[meio]
+        if chute == item:
+            return meio
+        if chute > item:
+            alto = meio - 1
         else:
-            low = middle + 1
+            baixo = meio + 1
     return None
 
 
-number_list = [1, 3, 5, 7, 9]
+lista_de_numeros = [1, 3, 5, 7, 9]
 
-print(binary_search(number_list, 3))
-print(binary_search(number_list, -1))
+print(BuscaBinaria(lista_de_numeros, 3))
+print(BuscaBinaria(lista_de_numeros, -1))
