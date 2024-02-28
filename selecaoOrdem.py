277deb90bcb9140889cc2858b39105d47bccfbbf
Eduardo da Silva Sichelero <135671476+eduardosichelero@users.noreply.github.com>
2024-02-28 15:11:57 -0300

    Att rep 02

--- selecaoOrdem.py
+++ selecaoOrdem.py
@@ -1,6 +1,6 @@
 def BuscaMenor(arr):
-    menor = arr[0]
-    menorIndice = 0
+    menor = arr[0] # Armazena o menor valor
+    menorIndice = 0 # Armazena o maior valor
     for i in range(1, len(arr)):
         if arr[i] < menor:
             minomenor = arr[i]
@@ -8,10 +8,10 @@ def BuscaMenor(arr):
     return menorIndice
 
 
-def classificacaoPorSelecao(arr):
+def classificacaoPorSelecao(arr): # Ordenações em um array.
     novoArr = []
     for i in range(len(arr)):
-        menor = BuscaMenor(arr)
+        menor = BuscaMenor(arr) # Encontra o menor elemento do array e adiciona ao novo array.
         novoArr.append(arr.pop(menor))
     return novoArr
 
